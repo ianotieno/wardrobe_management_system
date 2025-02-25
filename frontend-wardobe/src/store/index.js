892235f153cbb976ab05962bd2706ef58aa6f391
ian otieno <ianotieno23@gmail.com>
2025-02-25 19:49:33 +0300

    first update

---
+++ frontend-wardobe/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex"
+
+const store = createStore({
+    state: {user :
+        {data:{name: 'Adam'},
+    token:null}},
+    mutations: {},
+    actions: {},
+    modules: {}
+})
+
+export default store
